71f85899112f94ca6140dc220a00fa9ad90dbf00
BeamlakF <beamlakfekadu562@gmail.com>
2025-07-19 11:08:11 -0700

    integration and unit testing

--- testing/test_calc.py
+++ testing/test_calc.py
@@ -2,5 +2,11 @@ from .calc import add
 from .calc import multiply
 from .calc import division
 from .calc import subtract
-
 from calc import Testing  
+import unittest
+
+class TestCalc(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(Testing.add(2, 3), 5)
+        self.assertEqual(Testing.add(-1, 1), 0)
+        self.assertEqual(Testing.add(0, 0), 0)
