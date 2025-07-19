1c99ba98efbd97ddff3cab0912b485fefbde38f4
BeamlakF <beamlakfekadu562@gmail.com>
2025-07-19 10:19:55 -0700

    integration and unit testing

---
+++ testing/calc.py
@@ -0,0 +1,15 @@
+class testing:
+    def add(a,b):
+        return a+b
+    
+    def subtract(a,b):
+        return a-b
+    
+    def multiply(a,b):
+        return a*b
+    
+    def division(a,b):
+        if b != 0:
+            return a/b
+        else:
+            return False
---
+++ testing/test_calc.py
@@ -0,0 +1,5 @@
+from .calc import add 
+from .calc import multiply
+from .calc import division
+from .calc import subtract
+
