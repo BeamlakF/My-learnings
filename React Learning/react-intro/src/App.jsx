43d360387737b5cf8e0437f9ba81906c3ddcbc5c
BeamlakF <beamlakfekadu562@gmail.com>
2025-07-30 15:03:40 +0300

    name changer

--- React Learning/react-intro/src/App.jsx
+++ React Learning/react-intro/src/App.jsx
@@ -1,35 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState('')
+  const [bgColor, setbgColor] = useState ('')
+
+  const toggleColor = () => {
+    setbgColor (prev => ( prev === 'white' ? '#9e2222ff' : 'white'));
+  } 
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    
+      <div style={{ backgroundColor: bgColor, padding: '20px' }} >
+        <h1> Hello, {name || 'Stranger'}</h1>
+        <input type = "text"
+        placeholder='Enter you name'
+        value={name}
+        onChange={(e) => setName(e.target.value)}/>
+        <button onClick={toggleColor}>
+  Change Background
+</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      )
+        
 }
 
 export default App
