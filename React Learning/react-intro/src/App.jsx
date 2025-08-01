01de6440af4b736adbf6c25599eda645a14dad90
BeamlakF <beamlakfekadu562@gmail.com>
2025-08-01 20:39:15 +0300

    todo app simple

--- React Learning/react-intro/src/App.jsx
+++ React Learning/react-intro/src/App.jsx
@@ -3,24 +3,39 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [name, setName] = useState('')
-  const [bgColor, setbgColor] = useState ('')
+  // const [name, setName] = useState('')
+  // const [bgColor, setbgColor] = useState ('')
+  const [Todos, setTodos] = useState ([])
+  const [bgColor, setbgColor] = useState('white');
+  const [newTodo, setNewTodo] = useState('')
+  
 
-  const toggleColor = () => {
-    setbgColor (prev => ( prev === 'white' ? '#9e2222ff' : 'white'));
-  } 
+  // const toggleColor = () => {
+  //   setbgColor (prev => ( prev === 'white' ? '#9e2222ff' : 'white'));
+  // } 
+  const addTodo = () => {
+  if (newTodo.trim() === '') return; // don’t add empty tasks
+
+  const newItem = {
+    id: Date.now(),      // unique ID
+    text: newTodo,       // task text
+    done: false          // not completed yet
+  };
+
+  setTodos([...Todos, newItem]);  // add to the list
+  setNewTodo('');                 // clear input
+};
 
   return (
     
       <div style={{ backgroundColor: bgColor, padding: '20px' }} >
-        <h1> Hello, {name || 'Stranger'}</h1>
+        
         <input type = "text"
-        placeholder='Enter you name'
-        value={name}
-        onChange={(e) => setName(e.target.value)}/>
-        <button onClick={toggleColor}>
-  Change Background
-</button>
+        placeholder='Enter you task'
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}/>
+        <button onClick={addTodo}>
+         Add</button>
       </div>
 
       )
