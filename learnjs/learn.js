9a43ee5aef746ec678baab68b4aefdd08cab7f5c
BeamlakF <beamlakfekadu562@gmail.com>
2025-10-14 17:12:16 +0300

    js, learning using a todo app

--- learnjs/learn.js
+++ learnjs/learn.js
@@ -1,24 +1,31 @@
-let tasks = [];
-// use array to stote all the tasks, to store data
-tasks.push('eat');
-tasks.push('drink');
-// this is not reassigning, I am just mutating or changing my array value
-tasks =[];
-console.log(tasks);
-// Dom nodes --> input and addBtn
-
 const input = document.getElementById('task-input');
 const addBtn = document.getElementById('add-btn');
+const list = document.getElementById('task-list');
+// first we call the html elements by using the DOM tree structure
+// document
+//  └── input (id="taskInput") → has property .value
+//  └── button (id="addBtn") → has method .addEventListener()
+//  └── ul (id="taskList") → can hold <li> items
 
-console.log (input, addBtn);
-
-// adding event listeners do that out buttons actually function
 addBtn.addEventListener('click', function(){
-  const newTask = input.ariaValueMax.trim();
-  if (newTask !== ""){
-    tasks.push(newTask);
-    console.log(tasks);
-    input.value = '';
+  const taskText = input.value.trim();
+  if (taskText !== ''){
+    console.log("Task can not be empty");
+    return;
+
   }
-}
-)
+
+  const li = document.createElement('li');
+  li.textContent = taskText;
+  list.appendChild(li);
+  // DOM Method-Adds the new <li> into the <ul>
+  input.value = ''; //Clears the input field after adding
+
+  console.log (" you typed in:" + taskText);
+});
+// When this button is clicked, please call back this function.
+// function () {
+//   console.log("Button was clicked!");
+// } is called a callback function — a function that will be executed later by the browser when the event occurs.
+
+
